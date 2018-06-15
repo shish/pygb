cae09effcc0d0b56af35abf1d216f83d11e5872b
Shish <shish@shishnet.org>
2018-06-15 16:34:18 +0100

    more support for LCDC flags

--- lcd.py
+++ lcd.py
@@ -1,6 +1,12 @@
 import pygame
 
 VRAM_BASE = 0x8000
+TILE_DATA_TABLE_0 = 0x8800
+TILE_DATA_TABLE_1 = 0x8000
+BACKGROUND_MAP_0 = 0x9800
+BACKGROUND_MAP_1 = 0x9C00
+WINDOW_MAP_0 = 0x9800
+WINDOW_MAP_1 = 0x9C00
 SCALE = 2
 
 
@@ -59,36 +65,95 @@ class LCD:
 
         SCROLL_Y = self.cpu.ram[0xFF42]
         SCROLL_X = self.cpu.ram[0xFF43]
+        WND_Y = self.cpu.ram[0xFF4A]
+        WND_X = self.cpu.ram[0xFF4B]
+        LCDC = self.cpu.ram[0xFF40]
+
+        LCDC_ENABLED        = 0b10000000
+        LCDC_WINDOW_MAP     = 0b01000000
+        LCDC_WINDOW_ENABLED = 0b00100000
+        LCDC_DATA_SRC       = 0b00010000
+        LCDC_BG_MAP         = 0b00001000
+        LCDC_OBJ_SIZE       = 0b00000100
+        LCDC_OBJ_ENABLED    = 0b00000010
+        LCDC_BG_WIN_ENABLED = 0b00000001
+
         # print("SCROLL ", SCROLL_X, SCROLL_Y)
 
+        if LCDC & LCDC_DATA_SRC:
+            table = TILE_DATA_TABLE_1
+        else:
+            table = TILE_DATA_TABLE_0
         self.tiles = []
         for tile_id in range(0x200):
-            self.tiles.append(self.get_tile(tile_id, bgp))
+            self.tiles.append(self.get_tile(table, tile_id, bgp))
+
+        self.screen.fill(bgp[0])
 
         # Display only valid area
-        self.screen.fill(bgp[0])
         if self._game_only:
-            for y in range(144 // 8):
-                for x in range(160 // 8):
-                    tile_id = self.cpu.ram[0x9800 + y * 32 + x]
-                    self.screen.blit(self.tiles[tile_id], (x*8*SCALE - SCROLL_X*SCALE, y*8*SCALE - SCROLL_Y*SCALE))
 
+            # LCD enabled at all
+            if not LCDC & LCDC_ENABLED:
+                return True
+
+            # Background tiles
+            if LCDC & LCDC_BG_WIN_ENABLED:
+                if LCDC & LCDC_BG_MAP:
+                    background_map = BACKGROUND_MAP_1
+                else:
+                    background_map = BACKGROUND_MAP_0
+                for y in range(144 // 8):
+                    for x in range(160 // 8):
+                        tile_id = self.cpu.ram[background_map + y * 32 + x]
+                        self.screen.blit(self.tiles[tile_id], (x * 8 * SCALE - SCROLL_X * SCALE, y * 8 * SCALE - SCROLL_Y * SCALE))
+
+            # Window tiles
+            if LCDC & LCDC_WINDOW_ENABLED:
+                if LCDC & LCDC_WINDOW_MAP:
+                    window_map = WINDOW_MAP_1
+                else:
+                    window_map = WINDOW_MAP_0
+
+                for y in range(144 // 8):
+                    for x in range(160 // 8):
+                        tile_id = self.cpu.ram[window_map + y * 32 + x]
+                        self.screen.blit(self.tiles[tile_id], (x * 8 * SCALE + WND_X * SCALE, y * 8 * SCALE + WND_Y * SCALE))
+
+            # Sprites
+            if LCDC & LCDC_OBJ_ENABLED:
+                if LCDC & LCDC_OBJ_SIZE:
+                    size = (8, 16)
+                else:
+                    size = (8, 8)
+                raise Exception("Sprite mode enabled, but not implemented")
+
+        # Display all of VRAM
         else:
+            # Background memory
+            if LCDC & LCDC_BG_MAP:
+                background_map = BACKGROUND_MAP_1
+            else:
+                background_map = BACKGROUND_MAP_0
             for y in range(256 // 8):
                 for x in range(256 // 8):
-                    tile_id = self.cpu.ram[0x9800 + y * 32 + x]
-                    self.screen.blit(self.tiles[tile_id], (x*8*SCALE, y*8*SCALE))
+                    tile_id = self.cpu.ram[background_map + y * 32 + x]
+                    self.screen.blit(self.tiles[tile_id], (x * 8 * SCALE, y * 8 * SCALE))
+
+            # Background scroll border
             pygame.draw.rect(self.screen, pygame.Color(255, 0, 0), (SCROLL_X * SCALE, SCROLL_Y * SCALE, 160 * SCALE, 144 * SCALE), 1)
+
+            # Tile data
             for y in range(8):
                 for x in range(32):
-                    self.screen.blit(self.tiles[y*32+x], (256*SCALE + x*8*SCALE, y*8*SCALE))
+                    self.screen.blit(self.tiles[y * 32 + x], (256 * SCALE + x * 8 * SCALE, y * 8 * SCALE))
 
         pygame.display.update()
         self.clock.tick(60)
         return True
 
-    def get_tile(self, tile_id, pallette):
-        tile = self.cpu.ram[VRAM_BASE + tile_id * 16: VRAM_BASE + (tile_id * 16) + 16]
+    def get_tile(self, table, tile_id, pallette):
+        tile = self.cpu.ram[table + tile_id * 16: table + (tile_id * 16) + 16]
         surf = pygame.Surface((8 * SCALE, 8 * SCALE))
 
         for y in range(8):
